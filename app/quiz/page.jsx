f7e5c33b76167a598209b70141a323be933238eb
RaselMridha792 <raselmridha792@gmail.com>
2025-12-09 10:26:55 +0600

    repo private

--- app/quiz/page.jsx
+++ app/quiz/page.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div className='min-h-lvh text-center flex items-center justify-center bg-black text-white text-7xl'>quiz page hlw
+    <div className='min-h-lvh text-center flex items-center justify-center bg-black text-white text-7xl'>
+      this is the quiz page after repo private
     </div>
   )
 }
