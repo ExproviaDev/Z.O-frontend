31bbc2a0800725d83848cecefa4caa3041bc5565
RaselMridha792 <raselmridha792@gmail.com>
2025-12-09 16:03:33 +0600

    yml changes

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat eveniet provident, beatae maiores id cumque libero quas voluptas veniam earum?</div>
+    <div>adipisicing elit. Fugiat eveniet provident, beatae maiores id cumque libero quas voluptas veniam earum?</div>
   )
 }
