179978238d3971a0ad5bfdf47691d8796a5f4425
Sefat Ullah Fahad <mdf780709@gmail.com>
2025-12-10 14:49:25 +0600

    fixed simple issue

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" theme-data="light">
       <body
 
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
