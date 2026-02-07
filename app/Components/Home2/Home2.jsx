f7e460948ccb3de73ade060b2ca03a886c8e03eb
RaselMridha792 <raselmridha792@gmail.com>
2026-02-07 15:37:30 +0600

    implement ISR and SSR on homepage

--- app/Components/Home2/Home2.jsx
+++ app/Components/Home2/Home2.jsx
@@ -1,44 +1,36 @@
-import EventGallery from "../EventGallery/EventGallery"
-import JurySection from "../JuryCard/Jury"
-import HeroSection from "../HeroSection/Hero"
-import TimelineEvent from "../Timeline/TimelineEvent"
+import dynamic from "next/dynamic";
+
+export const revalidate = 86400; 
+const HeroSection = dynamic(() => import("../HeroSection/Hero"), {
+  loading: () => <div className="h-screen bg-gray-900 animate-pulse" />,
+});
+
+const Pricing = dynamic(() => import("../Pricing/Pricing"));
+const JurySection = dynamic(() => import("../JuryCard/Jury"));
+const EventGallery = dynamic(() => import("../EventGallery/EventGallery"));
+const Registration = dynamic(() => import("../Registration/Registration"));
 import Instructions from "../Instructions/Instructions";
 import WhyParticipate from "../WhyParticipate/WhyParticipate";
-import Registration from "../Registration/Registration";
-import Awards from "../Awards/Award";
-
-import Supporters from "../Supporters/Supporters";
-import Pricing from "../Pricing/Pricing";
 import SDGTable1 from "../InstructionPage/SDGTable1/SDGTable1";
-
-
+import Awards from "../Awards/Award";
+import TimelineEvent from "../Timeline/TimelineEvent";
+import Supporters from "../Supporters/Supporters";
 
 export default function HomePage() {
   return (
-    <main className="overflow-x-hidden">
+    <main className="overflow-x-hidden bg-white text-black">
+      <HeroSection />
+      <Instructions />
+      <WhyParticipate />
+      <SDGTable1 />
+      <Pricing />
+      <Registration />
+      <Awards />
+      <TimelineEvent />
+      <JurySection />
 
-      <HeroSection></HeroSection>
-      
-      <Instructions></Instructions>
-    
-      <WhyParticipate></WhyParticipate>
-
-      <SDGTable1></SDGTable1>
-
-      <Pricing></Pricing>
-
-      <Registration></Registration>
-
-      <Awards></Awards>
-      
-      <TimelineEvent></TimelineEvent>
-      
-      <JurySection></JurySection>
-
-      <Supporters></Supporters>
-     
-      <EventGallery></EventGallery>
-
+      <Supporters />
+      <EventGallery />
     </main>
   );
 }
